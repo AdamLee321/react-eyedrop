c4df9cae833cc4a91a3f73da45fcfd75210c2a69
AdamLee321 <adam123dublin@gmail.com>
2023-01-26 16:22:30 +0000

    Update: touchstart listener added for mobile

--- src/eyeDropper.tsx
+++ src/eyeDropper.tsx
@@ -1,20 +1,16 @@
-import * as React from 'react';
-import { parseRGB } from './colorUtils/parseRgb';
-import { rgbToHex } from './colorUtils/rgbToHex';
-import { OnChangeEyedrop, RgbObj, PickingMode } from './types';
-import { validatePickRadius } from './validations/validatePickRadius';
+import * as React from 'react'
+import { parseRGB } from './colorUtils/parseRgb'
+import { rgbToHex } from './colorUtils/rgbToHex'
+import { OnChangeEyedrop, RgbObj, PickingMode } from './types'
+import { validatePickRadius } from './validations/validatePickRadius'
 import { targetToCanvas } from './targetToCanvas'
 import { getColor } from './getColor'
 
-const {
-  useCallback,
-  useEffect,
-  useState
-} = React;
+const { useCallback, useEffect, useState } = React
 
 const styles = {
   eyedropperWrapper: {
-    position: 'relative'
+    position: 'relative',
   },
   eyedropperWrapperButton: {
     backgroundColor: '#000000',
@@ -22,33 +18,34 @@ const styles = {
     border: '1px solid #ffffff',
     borderRadius: '20%',
     padding: '10px 25px',
-  }
-} as const;
+  },
+} as const
 
 type Props = {
-  onChange: (changes: OnChangeEyedrop) => void,
-  wrapperClasses?: string,
-  buttonClasses?: string,
-  customComponent?: React.FC<any>,
-  once?: boolean,
-  cursorActive?: string,
-  cursorInactive?: string,
-  onInit?: () => void,
-  onPickStart?: () => void,
-  onPickEnd?: () => void,
-  colorsPassThrough?: string,
-  pickRadius?: number,
-  disabled?: boolean,
-  children?: React.ReactNode,
+  onChange: (changes: OnChangeEyedrop) => void
+  wrapperClasses?: string
+  buttonClasses?: string
+  customComponent?: React.FC<any>
+  once?: boolean
+  cursorActive?: string
+  cursorInactive?: string
+  onInit?: () => void
+  onPickStart?: () => void
+  onPickEnd?: () => void
+  colorsPassThrough?: string
+  pickRadius?: number
+  disabled?: boolean
+  children?: React.ReactNode
   customProps?: { [key: string]: any }
 }
 
-const initialStateColors = { rgb: '', hex: '' };
+const initialStateColors = { rgb: '', hex: '' }
 
 export const EyeDropper = (props: Props) => {
-  const [colors, setColors] = useState(initialStateColors);
-  const [pickingColorFromDocument, setPickingColorFromDocument] = useState(false);
-  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [colors, setColors] = useState(initialStateColors)
+  const [pickingColorFromDocument, setPickingColorFromDocument] =
+    useState(false)
+  const [buttonDisabled, setButtonDisabled] = useState(false)
   const {
     once = true,
     pickRadius = 0,
@@ -65,101 +62,121 @@ export const EyeDropper = (props: Props) => {
     disabled,
     onPickStart,
     onPickEnd,
-  } = props;
+  } = props
 
-  const setPickingMode = useCallback(({ isPicking, disableButton, showActiveCursor }: PickingMode) => {
-    if(document.body) {
-      document.body.style.cursor = showActiveCursor ? cursorActive : cursorInactive;
-    }
-    setButtonDisabled(disableButton);
-    window.setTimeout(() => {
-      setPickingColorFromDocument(isPicking);
-    }, 250)
-  }, [cursorActive, cursorInactive]);
+  const setPickingMode = useCallback(
+    ({ isPicking, disableButton, showActiveCursor }: PickingMode) => {
+      if (document.body) {
+        document.body.style.cursor = showActiveCursor
+          ? cursorActive
+          : cursorInactive
+      }
+      setButtonDisabled(disableButton)
+      window.setTimeout(() => {
+        setPickingColorFromDocument(isPicking)
+      }, 250)
+    },
+    [cursorActive, cursorInactive]
+  )
 
-  const deactivateColorPicking = useCallback(
-    () => {
-      setPickingMode({
-        isPicking: false,
-        disableButton: false,
-        showActiveCursor: false
-      })
-      onPickEnd && onPickEnd()
-    }, [setPickingMode, onPickEnd]
-  );
+  const deactivateColorPicking = useCallback(() => {
+    setPickingMode({
+      isPicking: false,
+      disableButton: false,
+      showActiveCursor: false,
+    })
+    onPickEnd && onPickEnd()
+  }, [setPickingMode, onPickEnd])
 
-  const exitPickByEscKey = useCallback((event: KeyboardEvent) => {
-    event.code === 'Escape' && pickingColorFromDocument && deactivateColorPicking()
-  }, [pickingColorFromDocument, deactivateColorPicking]);
+  const exitPickByEscKey = useCallback(
+    (event: KeyboardEvent) => {
+      event.code === 'Escape' &&
+        pickingColorFromDocument &&
+        deactivateColorPicking()
+    },
+    [pickingColorFromDocument, deactivateColorPicking]
+  )
 
   const pickColor = () => {
-    if (onPickStart) { onPickStart(); }
+    if (onPickStart) {
+      onPickStart()
+    }
 
     setPickingMode({
       isPicking: true,
       disableButton: disabled || true,
-      showActiveCursor: true
-    });
-  };
+      showActiveCursor: true,
+    })
+  }
 
-  const updateColors = useCallback((rgbObj: RgbObj) => {
-    const rgb = parseRGB(rgbObj);
-    const hex = rgbToHex(rgbObj);
+  const updateColors = useCallback(
+    (rgbObj: RgbObj) => {
+      const rgb = parseRGB(rgbObj)
+      const hex = rgbToHex(rgbObj)
 
-    // set color object to parent handler
-    onChange({ rgb, hex, customProps });
+      // set color object to parent handler
+      onChange({ rgb, hex, customProps })
 
-    setColors({ rgb, hex });
-  }, [customProps, onChange]);
+      setColors({ rgb, hex })
+    },
+    [customProps, onChange]
+  )
 
-  const extractColor = useCallback(async (e: MouseEvent) => {
-    const { target } = e
+  const extractColor = useCallback(
+    async (e: MouseEvent) => {
+      const { target } = e
 
-    if(!target) return
-    const targetCanvas = await targetToCanvas(target)
-    const rgbColor = getColor(targetCanvas, e, pickRadius)
+      if (!target) return
+      const targetCanvas = await targetToCanvas(target)
+      const rgbColor = getColor(targetCanvas, e, pickRadius)
 
-    updateColors(rgbColor)
-    once && deactivateColorPicking();
-  }, [deactivateColorPicking, once, pickRadius, updateColors]);
+      updateColors(rgbColor)
+      once && deactivateColorPicking()
+    },
+    [deactivateColorPicking, once, pickRadius, updateColors]
+  )
 
   useEffect(() => {
-    onInit && onInit();
-  }, [onInit]);
+    onInit && onInit()
+  }, [onInit])
 
   useEffect(() => {
-    pickRadius && validatePickRadius(pickRadius);
-  }, [pickRadius]);
+    pickRadius && validatePickRadius(pickRadius)
+  }, [pickRadius])
 
   // setup listener for canvas picking click
   useEffect(() => {
     if (pickingColorFromDocument) {
-      document.addEventListener('click', extractColor);
+      document.addEventListener('click', extractColor)
+      document.addEventListener('touchstart', extractColor)
     }
     return () => {
-      document.removeEventListener('click', extractColor);
-    };
-  }, [pickingColorFromDocument, once, extractColor]);
+      document.removeEventListener('click', extractColor)
+      document.removeEventListener('touchstart', extractColor)
+    }
+  }, [pickingColorFromDocument, once, extractColor])
 
   // setup listener for the esc key
   useEffect(() => {
     if (pickingColorFromDocument) {
-      document.addEventListener('keydown', exitPickByEscKey);
+      document.addEventListener('keydown', exitPickByEscKey)
     }
     return () => {
-      document.removeEventListener('keydown', exitPickByEscKey);
-    };
-  }, [pickingColorFromDocument, exitPickByEscKey]);
+      document.removeEventListener('keydown', exitPickByEscKey)
+    }
+  }, [pickingColorFromDocument, exitPickByEscKey])
 
   useEffect(() => {
     return () => {
-      if(document.body) {
+      if (document.body) {
         document.body.style.cursor = cursorInactive
       }
     }
   }, [])
 
-  const shouldColorsPassThrough = colorsPassThrough ? { [colorsPassThrough]: colors } : {};
+  const shouldColorsPassThrough = colorsPassThrough
+    ? { [colorsPassThrough]: colors }
+    : {}
   return (
     <div style={styles.eyedropperWrapper} className={wrapperClasses}>
       {CustomComponent ? (
@@ -171,7 +188,9 @@ export const EyeDropper = (props: Props) => {
         />
       ) : (
         <>
-          <style dangerouslySetInnerHTML={{__html: `
+          <style
+            dangerouslySetInnerHTML={{
+              __html: `
             .react-eyedrop-button {
               background-color: #000000;
               color: #ffffff;
@@ -179,7 +198,9 @@ export const EyeDropper = (props: Props) => {
               border-radius: 20%;
               padding: 10px 25px;
             }
-          `}} />
+          `,
+            }}
+          />
           <button
             id={'react-eyedrop-button'}
             className={`react-eyedrop-button ${buttonClasses || ''}`}
@@ -191,5 +212,5 @@ export const EyeDropper = (props: Props) => {
         </>
       )}
     </div>
-  );
-};
+  )
+}
